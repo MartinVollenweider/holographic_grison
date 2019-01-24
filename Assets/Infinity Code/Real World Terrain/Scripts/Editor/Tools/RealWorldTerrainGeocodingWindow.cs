3ec4969fae3066d71d9f48aff0675609cc554c8d
chrischne <chrischne@gmail.com>
2019-01-24 20:20:12 +0100

    create infinity code 3

---
+++ Assets/Infinity Code/Real World Terrain/Scripts/Editor/Tools/RealWorldTerrainGeocodingWindow.cs
@@ -0,0 +1,103 @@
+﻿/*     INFINITY CODE 2013-2018      */
+/*   http://www.infinity-code.com   */
+
+using InfinityCode.RealWorldTerrain.Webservices;
+using InfinityCode.RealWorldTerrain.Webservices.Results;
+using UnityEditor;
+using UnityEngine;
+
+namespace InfinityCode.RealWorldTerrain.Tools
+{
+    public class RealWorldTerrainGeocodingWindow : EditorWindow
+    {
+        private static RealWorldTerrainGeocodingWindow wnd;
+        private RealWorldTerrainMonoBase target;
+
+        private string response;
+        private Vector2 scrollPosition;
+        private string address;
+        private string languageCode = "en";
+        private GameObject resultGameObject;
+
+        private void OnDestroy()
+        {
+            wnd = null;
+        }
+
+        private void OnGUI()
+        {
+            target = EditorGUILayout.ObjectField("Container", target, typeof(RealWorldTerrainContainer), true) as RealWorldTerrainContainer;
+
+            address = EditorGUILayout.TextField("Location Name", address);
+            EditorGUILayout.BeginHorizontal();
+            languageCode = EditorGUILayout.TextField("Language Code", languageCode);
+            RealWorldTerrainGUIUtils.DrawHelpButton("List of Languages", "https://developers.google.com/maps/faq?hl=en#languagesupport");
+            EditorGUILayout.EndHorizontal();
+
+            if (GUILayout.Button("Search"))
+            {
+                RealWorldTerrainGoogleGeocoding.Find(new RealWorldTerrainGoogleGeocoding.GeocodingParams(address){language = languageCode}).OnComplete += OnRequestComplete;
+            }
+
+            EditorGUILayout.Space();
+
+            if (!string.IsNullOrEmpty(response))
+            {
+                GUILayout.Label("Full Response: ");
+                scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+                EditorGUILayout.TextArea(response);
+                EditorGUILayout.EndScrollView();
+            }
+        }
+
+        private void OnRequestComplete(string response)
+        {
+            this.response = response;
+            try
+            {
+                RealWorldTerrainGoogleGeocodingResult[] result = RealWorldTerrainGoogleGeocoding.GetResults(response);
+                if (result.Length > 0)
+                {
+                    Vector3 pos;
+                    target.GetWorldPosition(result[0].geometry_location, out pos);
+                    if (pos != default(Vector3))
+                    {
+                        resultGameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        resultGameObject.name = "Geocoding Result";
+                        resultGameObject.transform.position = pos;
+                        resultGameObject.GetComponent<MeshRenderer>().sharedMaterial.color = Color.red;
+                        RealWorldTerrainGeocodingObject geocodingObject = resultGameObject.AddComponent<RealWorldTerrainGeocodingObject>();
+                        geocodingObject.info = result[0];
+                        SceneView.onSceneGUIDelegate += OnSceneGUI;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            Repaint();
+        }
+
+        private void OnSceneGUI(SceneView view)
+        {
+            view.LookAt(resultGameObject.transform.position, view.camera.transform.rotation, 100);
+            SceneView.onSceneGUIDelegate -= OnSceneGUI;
+        }
+
+        [MenuItem("Window/Infinity Code/Real World Terrain/Tools/Geocoder")]
+        public static void OpenWindow()
+        {
+            OpenWindow(null);
+        }
+
+        public static void OpenWindow(RealWorldTerrainMonoBase target)
+        {
+            if (wnd != null) wnd.Close();
+
+            wnd = GetWindow<RealWorldTerrainGeocodingWindow>(true, "Geocoder");
+            if (target == null) wnd.target = FindObjectOfType<RealWorldTerrainContainer>();
+            else wnd.target = target;
+        }
+    }
+}
---
+++ Assets/Infinity Code/Real World Terrain/Scripts/Editor/Utils/RealWorldTerrainPackageManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+namespace InfinityCode.RealWorldTerrain
+{
+    public static class RealWorldTerrainPackageManager
+    {
+        [MenuItem("Window/Infinity Code/Real World Terrain/Packages/Playmaker Integration Kit")]
+        public static void ImportPlayMakerIntegrationKit()
+        {
+            RealWorldTerrainEditorUtils.ImportPackage("Packages\\RWT-Playmaker-Integration-Kit.unitypackage",
+                new RealWorldTerrainEditorUtils.Warning
+                {
+                    title = "Playmaker Integration Kit",
+                    message = "You have Playmaker in your project?",
+                    ok = "Yes, I have a Playmaker"
+                },
+                "Could not find Playmaker Integration Kit."
+            );
+        }
+
+    }
+}
